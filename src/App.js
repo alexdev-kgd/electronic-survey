5d914ffd33a1932a581a2275ce51869dd7393073
Alexdev2019 <alexpetrov248@gmail.com>
2021-09-20 13:50:12 +0200

    Survey Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.sass';
 import WelcomePage from './components/welcome-page/welcome-page';
 import EnterData from './components/enter-data/enter-data';
+import Survey from './components/survey/survey';
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
           <Route path="/" exact>
             <WelcomePage />
           </Route>
-          <Route path="/enterData" exact>
+          <Route path="/enterData">
             <EnterData />
+          </Route>
+          <Route path="/survey">
+            <Survey />
           </Route>
         </Switch>
       </div>
--- src/components/welcome-page/welcome-page.js
+++ src/components/welcome-page/welcome-page.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Button from '@material-ui/core/Button'
 import './welcome-page.sass'
 import { Link } from 'react-router-dom'
