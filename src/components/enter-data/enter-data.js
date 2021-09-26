0d28e1eaa31cad4777397c74fca7b004532f823c
Alexdev2019 <alexpetrov248@gmail.com>
2021-09-26 21:44:30 +0200

    Form Validation

--- src/components/enter-data/enter-data.js
+++ src/components/enter-data/enter-data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
@@ -5,28 +6,82 @@ import { useState } from 'react'
 import './enter-data.sass'
 
 const EnterData = () => {
-    const [ firstName, setFirstName] = useState('')
-    const [ lastName, setLastName] = useState('')
-    const [ age, setAge] = useState('')
+    const [values, setValues] = useState({})
+    const [errors, setErrors] = useState({})
+
+    let inputFields = [
+        {
+            name: "firstName",
+            id: "form__first-name",
+            label: "Your First Name"
+        },
+        {
+            name: "lastName",
+            id: "form__last-name",
+            label: "Your Last Name"
+        },
+        {
+            name: "age",
+            id: "form__age",
+            label: "Your Age"
+        },
+    ]
 
     let submitForm = event => {
         event.preventDefault()
 
-        localStorage.setItem("FirstName", firstName)
-        localStorage.setItem("LastName", lastName)
-        localStorage.setItem("Age", age)
+        if (isFormValid) {
+            for (let key of Object.keys(values)) {
+                localStorage.setItem(key, values[key])
+            }
+        }
     }
 
-    let changeFirstNameHandler = event => {
-        setFirstName(event.target.value)
+    let validateObjectValues = (item, conditional) => Object.values(item).every(conditional.bind(this));
+
+    let isFormValid = _ => {
+        if(Object.values(values).length <= 0) return false
+        const isError = validateObjectValues(errors, (x) => x === "")
+        const isFilled = validateObjectValues(values, (x) => x.length > 0);
+
+        return isError && isFilled
     }
 
-    let changeLastNameHandler = event => {
-        setLastName(event.target.value)
+    let isTextInputValid = text => text.match('^[a-zA-Z]*$') ? true : false
+
+    let isNumericInputValid = number => (number.match('^[0-9]*$') && number > 0 && number < 120) ? true : false
+
+    let handleInputValue = event => {
+        const { name, value } = event.target
+        setValues({
+            ...values,
+            [name]: value
+        })
+        validate({[name]: value})
     }
 
-    let changeAgeHandler = event => {
-        setAge(event.target.value)
+    let validate = inputField => {
+        let temp = { ...errors }
+        
+        if("firstName" in inputField) {
+            (inputField.firstName.length && isTextInputValid(inputField.firstName))
+            ? temp.firstName = ""
+            : temp.firstName = inputField.firstName
+        }
+
+        if("lastName" in inputField) {
+            (inputField.lastName.length && isTextInputValid(inputField.lastName))
+            ? temp.lastName = ""
+            : temp.lastName = inputField.lastName
+        }
+
+        if("age" in inputField) {
+            (inputField.age.length && isNumericInputValid(inputField.age))
+            ? temp.age = ""
+            : temp.age = inputField.age
+        }
+
+        setErrors({ ...temp })
     }
 
     return (
@@ -34,31 +89,25 @@ const EnterData = () => {
             <h1>First, Introduce Yourself</h1>
             <form className="enterData__form" onSubmit={submitForm}>
                 <div className="form">
-                    <TextField className="form__textfield"
-                               id="form__first-name"
-                               label="Your First Name"
-                               variant="outlined"
-                               value={firstName}
-                               onChange={changeFirstNameHandler}
-                               required />
-                    <TextField className="form__textfield"
-                               id="form__last-name"
-                               label="Your Last Name"
-                               variant="outlined"
-                               value={lastName}
-                               onChange={changeLastNameHandler}
-                               required />
-                    <TextField className="form__textfield"
-                               id="form__age"
-                               label="Your age"
-                               variant="outlined"
-                               value={age}
-                               onChange={changeAgeHandler}
-                               required />
+                    {inputFields.map((value, index) => {
+                        return (
+                            <TextField key={index}
+                                       {...(errors[value.name] && { error: true })}
+                                       name={value.name}
+                                       className="form__textfield"
+                                       id={value.id}
+                                       label={value.label}
+                                       variant="outlined"
+                                       onChange={handleInputValue}
+                                       required
+                            />
+                        )
+                    })}
                     <Button className="form__submit-btn"
                             type="submit"
                             variant="contained"
-                            color="primary">Begin</Button>
+                            color="primary"
+                            disabled={!isFormValid()}>Begin</Button>
                 </div>
             </form>
         </div>
