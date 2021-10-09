b16f05b9ae4fb2644e9256e4ed212879441115e8
Alexdev2019 <alexpetrov248@gmail.com>
2021-10-09 23:51:12 +0200

    Data reading from JSON file

--- src/components/survey/survey.js
+++ src/components/survey/survey.js
@@ -1,8 +1,69 @@
 import { FormControl, FormControlLabel, RadioGroup, Radio, Button } from "@material-ui/core"
 import TextField from '@material-ui/core/TextField';
 import './survey.sass'
+import data from '../../assets/survey.json';
 
 const Survey = () => {
+    const placeholder = "Enter text..."
+    const textFieldVariant = "outlined"
+
+    const getCheckboxAnswers = (answers) => {
+        return answers.map((answer, index) => {
+            return (
+                <FormControlLabel key={index} value={index} control={<Radio color="primary" />} label={answer.text} />
+            )
+        })
+    }
+
+    const getElementByType = (data) => {
+        switch (data.type) {
+            case 'text':
+                return (
+                    <TextField name={"question-" + data.id}
+                               variant={textFieldVariant}
+                               placeholder={placeholder}
+                    />
+                )
+            case 'textarea':
+                return (
+                    <TextField name={"question-" + data.id}
+                               multiline
+                               rows={5}
+                               variant={textFieldVariant}
+                               placeholder={placeholder}
+                    />
+                )
+            case 'numeric':
+                return (
+                    <TextField name={"question-" + data.id}
+                                type="number"
+                                inputProps={{ min: "0", max: "100", step: "1" }}
+                                variant={textFieldVariant}
+                                placeholder={placeholder}
+                    />
+                )
+            case 'checkbox':
+                return (
+                    <RadioGroup row name={"question-" + data.id}>
+                        { getCheckboxAnswers(data.answers) }
+                    </RadioGroup>
+                )
+            default:
+                break;
+        }
+    }
+
+    const readJSONfile = data.map((data) => {
+        return (
+            <div className="survey__block" key={data.id}>
+                <p className="survey__question">{data.question}</p>
+                <FormControl className="survey__answers" component="fieldset">
+                    { getElementByType(data) }
+                </FormControl>
+            </div>
+        )
+    })
+
     return (
         <div className="survey">
             <div className="survey__info-block">
@@ -12,46 +73,7 @@ const Survey = () => {
                 </p>
             </div>
             <div className="survey__container">
-                <div className="survey__block">
-                    <p className="survey__question">Lorem ipsum dolor sit amet?</p>
-                    <FormControl className="survey__answers" component="fieldset">
-                        <TextField name="question-1"
-                                   multiline
-                                   rows={5}
-                                   variant="outlined"
-                                   placeholder="Enter Text..."
-                        />
-                    </FormControl>
-                </div>
-                <div className="survey__block">
-                    <p className="survey__question">Lorem ipsum dolor sit amet?</p>
-                    <FormControl className="survey__answers" component="fieldset">
-                        <TextField name="question-1"
-                                   variant="outlined"
-                                   placeholder="Enter Text..."
-                        />
-                    </FormControl>
-                </div>
-                <div className="survey__block">
-                    <p className="survey__question">Lorem ipsum dolor sit amet?</p>
-                    <FormControl className="survey__answers" component="fieldset">
-                        <TextField name="question-1"
-                                   type="number"
-                                   inputProps={{ min: "0", max: "100", step: "1" }}
-                                   variant="outlined"
-                                   placeholder="Enter Number..."
-                        />
-                    </FormControl>
-                </div>
-                <div className="survey__block">
-                    <p className="survey__question">Lorem ipsum dolor sit amet?</p>
-                    <FormControl className="survey__answers" component="fieldset">
-                        <RadioGroup row  name="question-1">
-                            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
-                            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
-                        </RadioGroup>
-                    </FormControl>
-                </div>
+                { readJSONfile }
                 <div className="survey__submit-block">
                     <Button className="form__submit-btn"
                             type="submit"
