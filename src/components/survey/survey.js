010b15d10a40dac79888990ab513e4d0b8d5140a
Alexdev2019 <alexpetrov248@gmail.com>
2021-10-09 22:15:40 +0200

    Input Fields

--- src/components/survey/survey.js
+++ src/components/survey/survey.js
@@ -1,4 +1,5 @@
 import { FormControl, FormControlLabel, RadioGroup, Radio, Button } from "@material-ui/core"
+import TextField from '@material-ui/core/TextField';
 import './survey.sass'
 
 const Survey = () => {
@@ -14,19 +15,32 @@ const Survey = () => {
                 <div className="survey__block">
                     <p className="survey__question">Lorem ipsum dolor sit amet?</p>
                     <FormControl className="survey__answers" component="fieldset">
-                        <RadioGroup row  name="question-1">
-                            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
-                            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
-                        </RadioGroup>
+                        <TextField name="question-1"
+                                   multiline
+                                   rows={5}
+                                   variant="outlined"
+                                   placeholder="Enter Text..."
+                        />
                     </FormControl>
                 </div>
                 <div className="survey__block">
                     <p className="survey__question">Lorem ipsum dolor sit amet?</p>
                     <FormControl className="survey__answers" component="fieldset">
-                        <RadioGroup row  name="question-1">
-                            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
-                            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
-                        </RadioGroup>
+                        <TextField name="question-1"
+                                   variant="outlined"
+                                   placeholder="Enter Text..."
+                        />
+                    </FormControl>
+                </div>
+                <div className="survey__block">
+                    <p className="survey__question">Lorem ipsum dolor sit amet?</p>
+                    <FormControl className="survey__answers" component="fieldset">
+                        <TextField name="question-1"
+                                   type="number"
+                                   inputProps={{ min: "0", max: "100", step: "1" }}
+                                   variant="outlined"
+                                   placeholder="Enter Number..."
+                        />
                     </FormControl>
                 </div>
                 <div className="survey__block">
