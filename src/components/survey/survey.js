47714a2a570865155ce20e4d915f3e9eb7695f3a
alexdev-kgd <alexpetrov248@gmail.com>
2022-05-20 20:45:17 +0200

    Survey Form Validation

--- src/components/survey/survey.js
+++ src/components/survey/survey.js
@@ -3,16 +3,22 @@ import TextField from '@material-ui/core/TextField';
 import './survey.sass'
 import data from '../../assets/survey.json';
 import { useState } from "react";
+import { useHistory } from 'react-router';
 
 const Survey = () => {
+    const history = useHistory()
+
+    const surveyElemLength = data.length;
     const placeholder = "Enter text..."
     const textFieldVariant = "outlined"
+    const nameFormat = (index) => 'question-' + index;
     
     const [inputSet, setInputValue] = useState([]);
+    const [errors, setErrors] = useState({})
 
     const onInputChange = (e) => {
         const { type, name, value } = e.target;
-        // const isChecked = type === 'radio' ? e.target.checked : undefined;
+
         const newObjectStructure = {
             name: name,
             type: type,
@@ -33,16 +39,61 @@ const Survey = () => {
                 newObjectStructure
             ]);
         }
+        validate(newObjectStructure);
     }
 
     const inputReturnValue = (index) => {
         const filtered = inputSet.filter(item => item.name === 'question-'+index)[0]?.value;
-        console.log(filtered);
         return filtered || "";
+    }
+
+    const validateObjectValues = (item, conditional) => Object.values(item).every(conditional.bind(this));
+
+    const isFormValid = _ => {
+        if (inputSet.length !== surveyElemLength) return false;
+        let isValid;
+        inputSet.forEach(input => {
+            if(input.value.length === 0) return false;
+            isValid = validateObjectValues(errors, (x) => x === "");
+        });
+        return isValid;
+    }
+
+    const isTextInputValid = text => text.match('^[a-zA-Z]*$') ? true : false
+
+    const isNumericInputValid = number => (number.match('^[0-9]*$') && number > 0 && number < 120) ? true : false
+
+    const validate = input => {
+        let temp = { ...errors }
+
+        switch (input?.type) {
+            case "textarea":
+            case "text":
+                (input.value.length && isTextInputValid(input.value))
+                ? temp[input.name] = ""
+                : temp[input.name] = input.value
+                break;
+            case "number":
+                (input.value.length && isNumericInputValid(input.value))
+                ? temp[input.name] = ""
+                : temp[input.name] = input.value
+                break;
+            case "radio":
+                input.value.length ? temp[input.name] = "" : temp[input.name] = input.value
+                break;
+            default:
+                break;
+        }
+
+        setErrors({ ...temp })
     }
 
     const submitForm = event => {
         event.preventDefault()
+        console.log(inputSet);
+        if (isFormValid) {
+            history.push("/finish-page")
+        }
     }
 
     const getCheckboxAnswers = (answers, groupId) => {
@@ -62,41 +113,49 @@ const Survey = () => {
         switch (data.type) {
             case 'text':
                 return (
-                    <TextField name={"question-" + index}
+                    <TextField name={nameFormat(index)}
+                               {...(errors[nameFormat(index)] && { error: true })}
                                variant={textFieldVariant}
                                placeholder={placeholder}
                                value={inputReturnValue(index)}
                                onChange={onInputChange}
+                               required
                     />
                 )
             case 'textarea':
                 return ( 
-                    <TextField name={"question-" + index}
+                    <TextField name={nameFormat(index)}
+                               {...(errors[nameFormat(index)] && { error: true })}
                                multiline
                                rows={5}
                                variant={textFieldVariant}
                                placeholder={placeholder}
                                value={inputReturnValue(index)}
                                onChange={onInputChange}
+                               required
                     />
                 )
             case 'numeric':
                 return (
-                    <TextField name={"question-" + index}
+                    <TextField name={nameFormat(index)}
+                               {...(errors[nameFormat(index)] && { error: true })}
                                 type="number"
                                 inputProps={{ min: "0", max: "100", step: "1" }}
                                 variant={textFieldVariant}
                                 placeholder={placeholder}
                                 value={inputReturnValue(index)}
                                 onChange={onInputChange}
+                                required
                     />
                 )
             case 'checkbox':
                 return (
                     <RadioGroup row 
-                                name={"question-" + index}
+                                name={nameFormat(index)}
+                                {...(errors[nameFormat(index)] && { error: true })}
                                 onChange={onInputChange}
-                                value={""}>
+                                value={""}
+                                required>
                         { getCheckboxAnswers(data.answers, index) }
                     </RadioGroup>
                 )
@@ -121,18 +180,20 @@ const Survey = () => {
             <div className="survey__info-block">
                 <p>
                     For every question, you should choose to select only one answer. All of the questions need to be
-                    answered. As you finished answering questions, submit your answers by clicking on button "Submit"
+                    answered. As you finished answering questions, submit your answers by clicking the button "Submit"
                 </p>
             </div>
             <div className="survey__container">
-                { readJSONfile }
-                <div className="survey__submit-block">
-                    <Button onClick={submitForm}
-                            className="form__submit-btn"
-                            type="submit"
-                            variant="contained"
-                            color="primary">Submit</Button>
-                </div>
+                <form onSubmit={submitForm}>
+                    { readJSONfile }
+                    <div className="survey__submit-block">
+                        <Button className="form__submit-btn"
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                disabled={!isFormValid()}>Submit</Button>
+                    </div>
+                </form>
             </div>
         </div>
     )
